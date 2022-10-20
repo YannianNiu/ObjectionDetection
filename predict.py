7d9274b73d92431a6496ccfa2fc75c56cfa4402e
YannianNiu <yannianniu@gmail.com>
2022-10-20 20:43:31 +0800

    init commit

---
+++ predict.py
@@ -0,0 +1,19 @@
+#-------------------------------------#
+#       对单张图片进行预测
+#       输入图片的名字，就可检测
+#-------------------------------------#
+from yolo import YOLO
+from PIL import Image
+
+yolo = YOLO()
+
+while True:
+    img = input('Input image filename:')
+    try:
+        image = Image.open(img)
+    except:
+        print('Open Error! Try again!')
+        continue
+    else:
+        r_image = yolo.detect_image(image)
+        r_image.show()
